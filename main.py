276f5d148d70b66bb98850629b27fde6c5823780
markunnovel-ui <markun.novel@gmail.com>
2026-06-15 21:58:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def generate_rss(work_title, episode_title, episode_date):
     
     fe = fg.add_entry()
     fe.title(episode_title)
+    fe.published(episode_date) # これを追加（または変更）
     fe.updated(episode_date)
     fe.link(href=WORK_URL)
     fe.description(f"更新日: {episode_date}")
